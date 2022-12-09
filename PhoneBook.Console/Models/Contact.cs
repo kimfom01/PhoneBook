e523ca1bfeccdabcb64f80c9de6315cd17d1c590
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-12-09 09:55:06 +0300

    Add initial migrations

--- PhoneBook.Console/Models/Contact.cs
+++ PhoneBook.Console/Models/Contact.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PhoneBook.Console.Models;
 
 public class Contact
 {
+    [Key]
     public int Id { get; set; }
     public string Name { get; set; }
     public string PhoneNumber { get; set; }
