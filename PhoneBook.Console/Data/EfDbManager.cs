f72494a6dd0a20a823c60bad77e9e74b46b8f2ad
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-12-09 10:04:15 +0300

    Add entityframework implementation of DbManager

---
+++ PhoneBook.Console/Data/EfDbManager.cs
@@ -0,0 +1,26 @@
+using PhoneBook.Console.Models;
+
+namespace PhoneBook.Console.Data;
+
+public class EfDbManager : IDbManager
+{
+    public void AddNewContact(Contact newContact)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateContact(Contact oldContact, Contact newContact)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteContact(Contact contact)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Contact> GetContacts()
+    {
+        throw new NotImplementedException();
+    }
+}
