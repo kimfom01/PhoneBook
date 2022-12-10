a6196164f14ef971988f0f6738f58be206f6c107
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-12-11 00:33:34 +0300

    Add "Add Email Property Migrations"

--- PhoneBookConsole/Migrations/20221210213141_Add Email Property.Designer.cs
+++ PhoneBookConsole/Migrations/20221210213141_Add Email Property.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PhoneBookConsole.DbContexts;
 
@@ -10,9 +11,11 @@ using PhoneBookConsole.DbContexts;
 namespace PhoneBookConsole.Migrations
 {
     [DbContext(typeof(ContactContext))]
-    partial class ContactContextModelSnapshot : ModelSnapshot
+    [Migration("20221210213141_Add Email Property")]
+    partial class AddEmailProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
