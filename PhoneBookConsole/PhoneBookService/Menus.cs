98a3dfb43dc5f411c91a8b270ed3fb64ec83b030
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-12-10 19:50:55 +0300

    Add question to the main menu

--- PhoneBookConsole/PhoneBookService/Menus.cs
+++ PhoneBookConsole/PhoneBookService/Menus.cs
@@ -11,5 +11,7 @@ public class Menus
         Console.WriteLine("3 to edit a contact");
         Console.WriteLine("4 to delete a contact");
         Console.WriteLine("0 to close the app");
+        Console.WriteLine("\nType your choice and hit Enter");
+        Console.Write("Your choice? ");
     }
 }
