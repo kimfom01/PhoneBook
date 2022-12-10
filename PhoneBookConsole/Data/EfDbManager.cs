4384bb27c9e14eb391b629a2a36ced1e2f70ac9e
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-12-10 20:07:04 +0300

    Implement EntityFramework DbManager class

--- PhoneBookConsole/Data/EfDbManager.cs
+++ PhoneBookConsole/Data/EfDbManager.cs
@@ -1,3 +1,4 @@
+using PhoneBookConsole.DbContexts;
 using PhoneBookConsole.Models;
 
 namespace PhoneBookConsole.Data;
@@ -6,21 +7,41 @@ public class EfDbManager : IDbManager
 {
     public void AddNewContact(Contact newContact)
     {
-        throw new NotImplementedException();
+        using (var db = new ContactContext())
+        {
+            db.Add(newContact);
+            db.SaveChanges();
+        }
     }
 
     public void UpdateContact(Contact oldContact, Contact newContact)
     {
-        throw new NotImplementedException();
+        using (var db = new ContactContext())
+        {
+            var old = db.Contacts.Where(x => x.Name == oldContact.Name).First();
+            old.Name = newContact.Name;
+            old.PhoneNumber = newContact.PhoneNumber;
+            db.SaveChanges();
+        }
     }
 
     public void DeleteContact(Contact contact)
     {
-        throw new NotImplementedException();
+        using (var db = new ContactContext())
+        {
+            var toBeDeleted = db.Contacts.Where(x => x.Name == contact.Name).First();
+            db.Remove(toBeDeleted);
+            db.SaveChanges();
+        }
     }
 
     public List<Contact> GetContacts()
     {
-        throw new NotImplementedException();
+        using (var db = new ContactContext())
+        {
+            var contactList = db.Contacts.ToList();
+
+            return contactList;
+        }
     }
 }
