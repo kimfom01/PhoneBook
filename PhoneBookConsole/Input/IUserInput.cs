9edfe4b3767876dbc52c5cd37040b4de94b75077
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-01-09 10:32:54 +0300

    Add special methods for getting name, phone number and email

--- PhoneBookConsole/Input/IUserInput.cs
+++ PhoneBookConsole/Input/IUserInput.cs
@@ -3,4 +3,7 @@ namespace PhoneBookConsole.Input;
 public interface IUserInput
 {
     public string GetInput();
+    public string GetName();
+    public string GetPhoneNumber();
+    public string GetEmail();
 }
